2e1c116f7899bd995943ab8633f39b8de6f5ea6b
SmileAK-47 <125081306@qq.com>
2022-04-17 00:48:11 +0800

    1231312

--- RunTest.py
+++ RunTest.py
@@ -1,6 +1,6 @@
 #encoding = utf - 8
 #测试提交
 from testScripts.TestSendMailWithAttachment import TestSendMailWithAttachment
-##测试
+##测试的发生大撒旦发射点东方阿斯蒂芬
 if __name__ =="__main__":
     TestSendMailWithAttachment()
